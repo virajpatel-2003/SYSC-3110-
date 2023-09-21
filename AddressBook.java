19b126d83d1a3993c4aed49ef66b12a9a1d78e35
Viraj <virajpatel3@cmail.carleton.ca>
2023-09-21 17:32:13 -0400

    Created BuddyInfo test Object

--- AddressBook.java
+++ AddressBook.java
@@ -23,6 +23,7 @@ public class AddressBook
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println();
 
     }
 }
