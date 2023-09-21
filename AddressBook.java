890ea955b2f26df4b3759acc7d5b4d7bc3bd7b21
Viraj <virajpatel3@cmail.carleton.ca>
2023-09-21 17:01:32 -0400

    Created main

---
+++ AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class AddressBook
+{
+    ArrayList <BuddyInfo> lst;
+    AddressBook()
+    {
+    lst = new ArrayList<BuddyInfo>();
+    }
+
+    void addBuddy()
+    {
+    }
+
+    void removeBuddy()
+    {
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
