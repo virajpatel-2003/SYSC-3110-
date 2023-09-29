f28fed7746303013ba437136c16e597167da7219
Viraj <virajpatel3@cmail.carleton.ca>
2023-09-29 15:07:17 -0400

    Created BuddyInfo test Object 2

--- AddressBook.java
+++ AddressBook.java
@@ -20,8 +20,12 @@ public class AddressBook
     public static void main(String[] args){
 
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        BuddyInfo buddy2 = new BuddyInfo("Tom", "Carleton", "613");
+
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
+        addressBook.addBuddy(buddy2);
+
         addressBook.removeBuddy(buddy);
         System.out.println();
 
